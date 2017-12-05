15ed9d02567fd4181121692a844d58102adfbdc1
Félix Gómez <felix.gomez.lopez@gmail.com>
2017-12-05 10:47:01 +0100

    Day 5 proposed solution

---
+++ day5/5_1.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def calculate_steps(jump_offsets):
+    position = 0
+    steps = 0
+    while position < len(jump_offsets):
+        new_position = position + jump_offsets[position]
+        jump_offsets[position] += 1
+        position = new_position
+        steps += 1
+    return steps
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    ([0, 3, 0, 1, -3], 5)
+])
+def test_calculate_steps(test_input, expected):
+    assert calculate_steps(test_input) == expected
+
+
+if __name__ == '__main__':
+    jump_offsets = list(map(int, open("input").read().splitlines()))
+    print("Number of steps : %d" % calculate_steps(jump_offsets))
